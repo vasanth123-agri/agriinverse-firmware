181530cfaa5afe54d866f21f5673cc8a568ff23c
AgriInverseIoT <agriinverseiot@gmail.com>
2026-08-16 15:08:26 +0530

    Delete src/valve_node/valve_node.ino

--- src/valve_node/valve_node.ino
+++
@@ -1,9 +0,0 @@
-// Placeholder — real valve node code goes here
-   void setup() {
-       Serial.begin(115200);
-       Serial.println("Valve node placeholder booted");
-   }
-
-   void loop() {
-       delay(1000);
-   }
